f97d1725d922fe88731f161cc28ab0214db4dc39
lazaroofarrill <lazaroofarrill@gmail.com>
2023-10-30 21:49:47 -0400

    changes

--- src/main.c
+++ src/main.c
@@ -16,77 +16,82 @@
 LOG_MODULE_REGISTER(app);
 
 int i2c_ping(const struct device *i2c_dev, uint16_t addr) {
-  const uint8_t msg[1] = {0};
+    const uint8_t msg[1] = {0};
 
-  return i2c_write(i2c_dev, msg, 1, addr);
+    return i2c_write(i2c_dev, msg, 1, addr);
 }
 
 void scan_i2c(const struct device *i2c_dev, const struct gpio_dt_spec led) {
-  for (uint8_t i = 3; i < 0x78; i++) {
-    int result = i2c_ping(i2c_dev, i);
+    for (uint8_t i = 3; i < 0x78; i++) {
+        int result = i2c_ping(i2c_dev, i);
 
-    if (i % 16 == 0)
-      printk("\n%.2x:", i);
+        if (i % 16 == 0)
+            printk("\n%.2x:", i);
 
-    if (result == 0) {
-      printk(" %.2x", i);
-    } else
-      printk(" --");
-  }
+        if (result == 0) {
+            printk(" %.2x", i);
+        } else
+            printk(" --");
+    }
 
-  k_msleep(1000);
-  gpio_pin_toggle_dt(&led);
+    k_msleep(1000);
+    gpio_pin_toggle_dt(&led);
 }
 
 // A build error here means the board is not supported
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main() {
-  int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0) {
+    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
+    const struct device *const i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2c0));
+    const struct device *const as5600_dev = DEVICE_DT_GET(DT_ALIAS(vane));
+    const struct device *const icm20948_dev = DEVICE_DT_GET(DT_ALIAS(compass));
+
+    printk("\n");
+
+    if (!device_is_ready(i2c_dev)) {
+        printk("Could not get i2c device\n");
+        return -1;
+    }
+
+    if (!device_is_ready(icm20948_dev)) {
+        printk("Could not get icm20948 sensor");
+        return -1;
+    }
+
+    printk("Using board %s\n", CONFIG_BOARD);
+    printk("Scanning i2c:\n");
+    scan_i2c(i2c_dev, led);
+    printk("\n");
+
+    //  struct RtcDs1307 *rtc = ds1307_create(i2c_dev, 0x68);
+
+    char axis[] = {'x', 'y', 'z'};
+
+    while (1) {
+        struct sensor_value angle;
+        struct sensor_value accel_xyz[3];
+
+        //    sensor_sample_fetch(as5600_dev);
+        //    sensor_channel_get(as5600_dev, SENSOR_CHAN_ROTATION, &angle);
+
+        sensor_sample_fetch(icm20948_dev);
+        sensor_channel_get(icm20948_dev, SENSOR_CHAN_ACCEL_XYZ, accel_xyz);
+        //    printk("angle: %d.%06d\n", angle.val1, angle.val2);
+        // ds1307_time_fetch(rtc);
+
+        for (int i = 0; i < 3; i++) {
+            printk("%c: %d.%d\t", axis[i], (accel_xyz + i)->val1, (accel_xyz + i)->val2);
+        }
+        printk("\n");
+
+        k_sleep(K_MSEC(1000));
+        gpio_pin_toggle_dt(&led);
+    }
+
     return 0;
-  }
-
-  const struct device *const i2c_dev = DEVICE_DT_GET(DT_ALIAS(i2c0));
-  const struct device *const as5600_dev = DEVICE_DT_GET(DT_ALIAS(vane));
-  const struct device *const icm20948_dev = DEVICE_DT_GET(DT_ALIAS(compass));
-
-  printk("\n");
-
-  if (!device_is_ready(i2c_dev)) {
-    printk("Could not get i2c device\n");
-    return -1;
-  }
-
-  if (!device_is_ready(icm20948_dev)) {
-    printk("Could not get icm20948 sensor");
-    return -1;
-  }
-
-  printk("Using board %s\n", CONFIG_BOARD);
-  printk("Scanning i2c:\n");
-  scan_i2c(i2c_dev, led);
-  printk("\n");
-
-  //  struct RtcDs1307 *rtc = ds1307_create(i2c_dev, 0x68);
-
-  while (1) {
-    struct sensor_value angle;
-    struct sensor_value accel_x;
-
-    //    sensor_sample_fetch(as5600_dev);
-    //    sensor_channel_get(as5600_dev, SENSOR_CHAN_ROTATION, &angle);
-
-    sensor_sample_fetch(icm20948_dev);
-    sensor_channel_get(icm20948_dev, SENSOR_CHAN_ACCEL_X, &accel_x);
-    //    printk("angle: %d.%06d\n", angle.val1, angle.val2);
-    // ds1307_time_fetch(rtc);
-    //
-    printk("x: %d.%d", accel_x.val1, accel_x.val2);
-
-    k_sleep(K_MSEC(1000));
-    gpio_pin_toggle_dt(&led);
-  }
-
-  return 0;
 }
